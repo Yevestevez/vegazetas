fd230bb6ec5b3277b12d924b55c2c49c7f85cf6b
Yevestevez <yevestevez.cg@gmail.com>
2025-06-11 17:37:20 +0200

    improve code order in logic/recoverPassword and fix syntax errors

--- api/logic/recoverPassword.js
+++ api/logic/recoverPassword.js
@@ -2,7 +2,7 @@ import crypto from 'crypto'
 import { User } from '../data/models.js'
 import { createTestMailTransporter } from '../util/mailer.js'
 import nodemailer from 'nodemailer'
-import {errors, validate} from 'com'
+import { errors, validate } from 'com'
 
 const { NotFoundError, SystemError } = errors
 
@@ -14,42 +14,36 @@ const recoverPassword = (email) => {
         .then(user => {
             if (!user) throw new NotFoundError('user not found')
 
-        const token = crypto.randomBytes(32).toString('hex')
-        const expires = Date.now() + 1000 * 60 * 15 // 15 minutos
+            const token = crypto.randomBytes(32).toString('hex')
+            const expires = Date.now() + 1000 * 60 * 15 // 15 minutos
 
-        user.resetPasswordToken = token
-        user.resetPasswordExpires = expires
+            user.resetPasswordToken = token
+            user.resetPasswordExpires = expires
 
-        return user.save()
-            .catch(error => { throw new SystemError(error.message) })
-            .then(() => {
-                return { user, token }
-            })
+            return user.save()
+                .catch(error => { throw new SystemError(error.message) })
+                .then(() => {
+                    return { user, token }
+                })
         })
-        .then(({ user, token}) => {
+        .then(({ user, token }) => {
             return createTestMailTransporter()
                 .catch(error => { throw new SystemError(error.message) })
                 .then(transporter => {
-                    return { transporter, user, token }
-                })
-    })
-        .then(({ transporter, user, token }) => {
-            const resetLink = `http://localhost:5173/reset-password/${token}`
+                    const resetLink = `http://localhost:5173/reset-password/${token}`
 
-            return transporter.sendMail({
-                from: '"Vegazetas" <no-reply@vegazetas.com>',
-                to: user.email,
-                subject: 'Recuperar contraseña',
-                text: 'Haz click aquí para cambiar tu contraseña: ${resetLink}',
-                html:`<p>Haz clic aquí para cambiar tu contraseña:</p><a href="${resetLink}">${resetLink}</a>`
-            })
-            .catch(error => { throw new SystemError(error.message) })
+                    return transporter.sendMail({
+                        from: '"Vegazetas" <no-reply@vegazetas.com>',
+                        to: user.email,
+                        subject: 'Recuperar contraseña',
+                        text: `Haz click aquí para cambiar tu contraseña: ${resetLink}`,
+                        html: `<p>Haz clic aquí para cambiar tu contraseña:</p><a href="${resetLink}">${resetLink}</a>`
+                    })
+                        .catch(error => { throw new SystemError(error.message) })
+                })
         })
         .then(info => {
             console.log('📨 Vista previa:', nodemailer.getTestMessageUrl(info))
-        })
-        .catch(error => {
-            throw new SystemError(error.message)
         })
 }
 
