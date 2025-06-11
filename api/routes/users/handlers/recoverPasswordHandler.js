d13363d416cc52fd949accb780a2258430c60a7e
Yevestevez <yevestevez.cg@gmail.com>
2025-06-11 13:36:13 +0200

    add recoverPasswordHandlÃer pointing to recoverPassword logic

---
+++ api/routes/users/handlers/recoverPasswordHandler.js
@@ -0,0 +1,17 @@
+import logic from '../../../logic/index.js'
+
+export default (req, res, next) => {
+    try {
+        const { email } = req.body
+
+        recoverPassword(email)
+            .then(() => {
+                res.status(200).json({ message: 'Correo de recuperación enviado' })
+            })
+            .catch(error => {
+                next(error)
+            })
+    } catch (error) {
+        next(error)
+    }
+}
