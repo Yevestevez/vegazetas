12d6efaa4df35caefb3947e467f393e96e3db287
Yevestevez <yevestevez.cg@gmail.com>
2025-06-11 13:50:09 +0200

    add recoverPasswordHandler to handlers index

--- api/routes/users/handlers/index.js
+++ api/routes/users/handlers/index.js
@@ -2,10 +2,12 @@ import authenticateUserHandler from './authenticateUserHandler.js'
 import registerUserHandler from './registerUserHandler.js'
 import getUserNameHandler from './getUserNameHandler.js'
 import getUserUsernameHandler from './getUserUsernameHandler.js'
+import recoverPasswordHandler from './recoverPasswordHandler.js'
 
 export {
     authenticateUserHandler,
     registerUserHandler,
     getUserNameHandler,
-    getUserUsernameHandler
+    getUserUsernameHandler,
+    recoverPasswordHandler
 }
