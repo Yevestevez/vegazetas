6ecb9a92b346827896137047b925f1b50afc01e8
Yevestevez <yevestevez.cg@gmail.com>
2025-06-11 13:57:24 +0200

    add password/recover to users router

--- api/routes/users/index.js
+++ api/routes/users/index.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { authenticateUserHandler, getUserNameHandler, registerUserHandler, getUserUsernameHandler } from './handlers/index.js'
+import { authenticateUserHandler, getUserNameHandler, registerUserHandler, getUserUsernameHandler, recoverPasswordHandler } from './handlers/index.js'
 import jsonBodyParser from '../../middlewares/jsonBodyParser.js'
 
 const router = new Router()
@@ -8,5 +8,6 @@ router.post('/auth', jsonBodyParser, authenticateUserHandler)
 router.get('/name', getUserNameHandler)
 router.get('/username', getUserUsernameHandler)
 router.post('/', jsonBodyParser, registerUserHandler)
+router.post('/password/recover', jsonBodyParser, recoverPasswordHandler)
 
 export default router
