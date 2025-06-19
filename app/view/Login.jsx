1d8b2a63d32c3c49e9ddb77c15c168c0e2239d96
Yevestevez <yevestevez.cg@gmail.com>
2025-06-19 10:38:25 +0200

    feat (Login): add server sleep alert with timeout in passwordRecover logic; use alert return to auto-close it on server response

--- app/view/Login.jsx
+++ app/view/Login.jsx
@@ -67,16 +67,31 @@ function Login({ onRegisterClicked, onUserLoggedIn }) {
                 const form = event.target
                 const email = form.email.value.trim()
 
+                let closeAlert
+
+                const loadingAlertTimeout = setTimeout(() => {
+                    closeAlert = alert('⏳ El servidor se está despertando, espera unos segundos...')
+                }, 2000)
+
                 try {
                     logic.passwordRecover(email)
                         .then(() => {
+                            clearTimeout(loadingAlertTimeout)
+                            if (closeAlert) closeAlert()
+
                             alert('Te hemos enviado un correo para recuperar tu contraseña')
                         })
                         .catch(error => {
+                            clearTimeout(loadingAlertTimeout)
+                            if (closeAlert) closeAlert()
+
                             alert(error.message)
                             console.error(error)
                         })
                 } catch (error) {
+                    clearTimeout(loadingAlertTimeout)
+                    if (closeAlert) closeAlert()
+
                     alert(error.message)
 
                     console.error(error)
