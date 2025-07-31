2f7fcf4018acad8ca4551eb48473959250404683
Yevestevez <yevestevez.cg@gmail.com>
2025-07-31 18:25:56 +0200

    general type improve and refactor

--- app/view/Home/MyRecipes/index.jsx
+++ app/view/Home/MyRecipes/index.jsx
@@ -12,7 +12,7 @@ function MyRecipes({ onRecipeThumbnailClick, onUserLoggedOut, onLogoClicked, onC
     const [myRecipes, setMyRecipes] = useState([])
 
     useEffect(() => {
-        console.log('MyRecipes -> "componentDidMount" (useEffect)')
+        console.log('MyRecipes -> useEffect')
 
         loadMyRecipes()
     }, [])
@@ -38,7 +38,7 @@ function MyRecipes({ onRecipeThumbnailClick, onUserLoggedOut, onLogoClicked, onC
     const handleRecipeThumbnailClick = (recipeId) => onRecipeThumbnailClick(recipeId)
     const handleUserLoggedOut = () => onUserLoggedOut()
 
-    const handleLogoLinkCLick = () => onLogoClicked()
+    const handleLogoLinkClick = () => onLogoClicked()
 
     const handleCreateRecipeClick = event => {
         event.preventDefault()
@@ -46,65 +46,16 @@ function MyRecipes({ onRecipeThumbnailClick, onUserLoggedOut, onLogoClicked, onC
         onCreateRecipeClicked()
     }
 
-    const btnClasses = `
-        /* Layout */
-        flex items-center justify-center rounded-full
-
-        /* Tamaño */
-        h-[20vw] w-[20vw]
-        sm:w-[18vw] sm:h-[18vw]
-        md:w-[14vw] md:h-[14vw]
-
-        /* Tipografía */
-        anybody-logo
-        text-[clamp(min(4vw,10rem),4vw,10rem)]/[100%]
-        md:text-[clamp(min(2vw,10rem),3vw,10rem)]
-
-        /* Sombra */
-        drop-shadow-[1.5vw_1.5vw_0_rgba(0,0,0,0.8)]
-        md:drop-shadow-[1.2vw_1.2vw_0_rgba(0,0,0,0.8)]
-
-        /* Animaciones */
-        transition-transform duration-150 ease-out
-
-        /* Hover */
-        hover:bg-aquamarine hover:text-folly
-        hover:drop-shadow-[1.7vw_1.7vw_0_rgba(0,0,0,0.7)]
-        hover:md:drop-shadow-[1.4vw_1.4vw_0_rgba(0,0,0,0.7)]
-        hover:-translate-y-2 hover:scale-105
-    `
-
     console.log('MyRecipes -> render')
 
-    return <div className="
-        /* Layout */
-        flex flex-col xl:flex-row min-h-screen w-full items-center text-center overflow-x-hidden
-
-        pt-[21vw] sm:pt-[17vw] md:pt-[16vw] xl:pt-[0vw]
-
-        /* Colores */
-        bg-canary
-    ">
+    return <div className="flex flex-col xl:flex-row min-h-screen w-full items-center text-center overflow-x-hidden pt-[21vw] sm:pt-[17vw] md:pt-[16vw] xl:pt-[0vw] bg-canary">
         <Header
             onUserLoggedOut={handleUserLoggedOut}
-            onLogoClicked={handleLogoLinkCLick}
+            onLogoClicked={handleLogoLinkClick}
         />
 
-        <div className="
-            flex xl:fixed xl:bottom-0 text-center py-[2vw] xl:py-[5vh] items-center xl:justify-end xl:px-[5vw]
-
-            xl:w-[40vw]
-            xl:h-[60vh]
-
-            xl:bg-sgbus-green
-        ">
-            <h1 className="
-                /* Tipografía */
-                anybody-logo text-folly xl:text-veronica text-[6vw] xl:text-[2.6vw]/[100%]
-
-                /* Sombra */
-                drop-shadow-[0.12em_0.12em_0_rgba(0,0,0,0.8)]
-            ">Tus <br className="hidden xl:block" />recetas</h1>
+        <div className="flex xl:fixed xl:bottom-0 text-center py-[2vw] xl:py-[5vh] items-center xl:justify-end xl:px-[5vw] xl:w-[40vw] xl:h-[60vh] xl:bg-sgbus-green">
+            <h1 className="anybody-logo text-folly xl:text-veronica text-[6vw] xl:text-[2.6vw]/[100%] drop-shadow-[0.12em_0.12em_0_rgba(0,0,0,0.8)]">Tus <br className="hidden xl:block" />recetas</h1>
         </div>
 
         <main className="flex flex-col w-full xl:w-[35vw] xl:ml-[40vw]">
@@ -120,21 +71,23 @@ function MyRecipes({ onRecipeThumbnailClick, onUserLoggedOut, onLogoClicked, onC
                 ))
             ) : (
                 <>
-                    <p className="
-                        /* Layout */
-                        text-center mt-[2vw]
+                    <p className="text-center mt-[2vw] anybody font-bold text-folly text-[4vw]/[120%] xl:text-[2vw]/[120%]">Todavía no tienes ninguna receta<br />¡Añade una nueva!</p>
+                    <button
+                        type='button'
+                        onClick={handleCreateRecipeClick}
+                        className="
+                            flex items-center justify-center rounded-full mx-auto text-center
+                            h-[20vw] sm:h-[18vw] md:h-[14vw] xl:h-[10vw]
+                            w-[20vw] sm:w-[18vw] md:w-[14vw] xl:w-[10vw]
 
-                        /* Tipografía */
-                        anybody font-bold text-folly text-[4vw]/[120%]
-                    ">Todavía no tienes ninguna receta<br />¡Añade una nueva!</p>
-                    <button type='button' className={`
-                        ${btnClasses}
-                        /* Layout */
-                        flex items-center mx-auto justify-center text-center
+                            anybody-logo text-[clamp(1rem,4vw,10rem)]/[100%] md:text-[clamp(1.5rem,3vw,10rem)] xl:text-[clamp(1rem,2vw,3rem)]
 
-                        /* Colores */
-                        bg-folly mt-[8vw] text-spring-bud
-                    `} onClick={handleCreateRecipeClick}>Nueva<br></br>receta</button>
+                            drop-shadow-[1.5vw_1.5vw_0_rgba(0,0,0,0.8)] md:drop-shadow-[1.2vw_1.2vw_0_rgba(0,0,0,0.8)]
+                            hover:drop-shadow-[1.7vw_1.7vw_0_rgba(0,0,0,0.7)] hover:md:drop-shadow-[1.4vw_1.4vw_0_rgba(0,0,0,0.7)]
+
+                            bg-folly mt-[8vw] text-spring-bud hover:bg-aquamarine hover:text-folly
+                            hover:-translate-y-2 hover:scale-105 transition-transform duration-150 ease-out
+                        ">Nueva<br></br>receta</button>
                 </>
             )}
         </main>
--- app/view/Landing.jsx
+++ app/view/Landing.jsx
@@ -13,114 +13,43 @@ function Landing({ onLoginClicked, onRegisterClicked }) {
 
     console.log('Landing -> render')
 
-    // Estilos comunes para botones
     const btnClasses = `
-        /* Layout */
         flex items-center justify-center rounded-full
-
-        /* Tamaño */
-        h-[26vw] w-[26vw]
-        sm:w-[22vw] sm:h-[22vw]
-        md:w-[18vw] md:h-[18vw]
-        lg:h-[18vw] lg:w-[18vw]
-        xl:h-[9vw] xl:w-[9vw]
-        2xl:h-[8vw] 2xl:w-[8vw]
-    
-        /* Colores */
-        bg-spring-bud text-folly
-    
-        /* Tipografía */
-        anybody-logo
-    
-        /* Sombra */
-        drop-shadow-[1.5vw_1.5vw_0_rgba(0,0,0,0.8)]
-        lg:drop-shadow-[1.2vw_1.2vw_0_rgba(0,0,0,0.8)]
-        xl:drop-shadow-[0.8vw_0.8vw_0_rgba(0,0,0,0.8)]
-        2xl:drop-shadow-[0.6vw_0.6vw_0_rgba(0,0,0,0.8)]
-    
-        /* Animaciones */
-        transition-transform duration-150 ease-out
-    
-        /* Hover */
-        hover:drop-shadow-[1.7vw_1.7vw_0_rgba(0,0,0,0.7)]
-        hover:lg:drop-shadow-[1.4vw_1.4vw_0_rgba(0,0,0,0.7)]
-        hover:xl:drop-shadow-[1vw_1vw_0_rgba(0,0,0,0.7)]
-        hover:2xl:drop-shadow-[0.8vw_0.8vw_0_rgba(0,0,0,0.7)]
-        hover:-translate-y-2 hover:scale-105
+        h-[26vw] sm:h-[22vw] md:h-[18vw] lg:h-[18vw] xl:h-[9vw] 2xl:h-[8vw]
+        w-[26vw] sm:w-[22vw] md:w-[18vw] lg:w-[18vw] xl:w-[9vw] 2xl:w-[8vw]
+        bg-spring-bud text-folly anybody-logo
+        drop-shadow-[1.5vw_1.5vw_0_rgba(0,0,0,0.8)] lg:drop-shadow-[1.2vw_1.2vw_0_rgba(0,0,0,0.8)] xl:drop-shadow-[0.8vw_0.8vw_0_rgba(0,0,0,0.8)] 2xl:drop-shadow-[0.6vw_0.6vw_0_rgba(0,0,0,0.8)]
+        hover:drop-shadow-[1.7vw_1.7vw_0_rgba(0,0,0,0.7)] hover:lg:drop-shadow-[1.4vw_1.4vw_0_rgba(0,0,0,0.7)] hover:xl:drop-shadow-[1vw_1vw_0_rgba(0,0,0,0.7)] hover:2xl:drop-shadow-[0.8vw_0.8vw_0_rgba(0,0,0,0.7)]
+        transition-transform duration-150 ease-out hover:-translate-y-2 hover:scale-105
     `
 
-    return <div className="
-        /* Layout */
-        flex flex-col min-h-screen min-w-screen items-center gap-[14vw] sm:gap-[10vw] md:gap-[8vw] xl:gap-[4vw] 2xl:gap-[3vw]
-
-        /* Colores */
-        bg-folly
-    ">
+    return <div className="flex flex-col min-h-screen min-w-screen items-center gap-[14vw] sm:gap-[10vw] md:gap-[8vw] xl:gap-[4vw] 2xl:gap-[3vw] bg-folly">
         <h1 className="
-            /* Layout */
-            text-center
-            mt-[38vw] sm:mt-[24vw] md:mt-[22vw] xl:mt-[6vw]
-    
-            /* Tipografía */
-            anybody-logo text-spring-bud
-            text-[30vw]/[80%] 
-            sm:text-[28vw]/[80%] 
-            md:text-[24vw]/[80%]
-            xl:text-[12vw]/[80%]
-            2xl:text-[11vw]/[80%]
-    
-            /* Sombra */
-            drop-shadow-[0.06em_0.06em_0_rgba(0,0,0,0.8)]
-        ">Vega<br></br>zetas</h1>
+                text-center mt-[38vw] sm:mt-[24vw] md:mt-[22vw] xl:mt-[6vw] anybody-logo text-spring-bud drop-shadow-[0.06em_0.06em_0_rgba(0,0,0,0.8)]
+                text-[30vw]/[80%] sm:text-[28vw]/[80%] md:text-[24vw]/[80%] xl:text-[12vw]/[80%] 2xl:text-[11vw]/[80%]
+            ">Vega<br></br>zetas
+        </h1>
 
         <p className="
-            /* Layout */
-            text-center
+                text-center anybody-title text-spring-bud
+                text-[8vw]/[120%] sm:text-[8vw]/[120%] md:text-[7vw]/[120%] xl:text-[4.5vw]/[120%] 2xl:text-[4vw]/[120%]
+            ">Cook & Cool
+        </p>
 
-            /* Tipografía */
-            anybody-title text-spring-bud
-            text-[8vw]/[120%] 
-            sm:text-[8vw]/[120%]
-            md:text-[7vw]/[120%]
-            xl:text-[4.5vw]/[120%]
-            2xl:text-[4vw]/[120%]
-        ">Cook & Cool</p>
-
-        <main className="
-            /* Layout */
-            flex flex-col sm:flex-row lg:flex-row md:flex-row 
-            items-center
-            gap-[8vw] xl:gap-[5vw] 2xl:gap-[4vw]
-        ">
+        <main className="flex flex-col sm:flex-row lg:flex-row md:flex-row items-center gap-[8vw] xl:gap-[5vw] 2xl:gap-[4vw]">
             <a className={`
-                ${btnClasses}
-                
-                /* Tipografía */
-                text-[clamp(min(7vw,10rem),7vw,10rem)] 
-                sm:text-[clamp(min(5vw,10rem),5vw,10rem)]
-                md:text-[clamp(min(2vw,10rem),4vw,10rem)]
-                xl:text-[clamp(min(2vw,10rem),2.2vw,10rem)]
-                2xl:text-[clamp(min(1.8vw,10rem),2vw,10rem)]
-
-                /* Hover */
-                hover:bg-violet hover:text-hot-magenta
-            `}
+                    ${btnClasses}
+                    text-[clamp(1.8rem,7vw,10rem)] sm:text-[clamp(5rem,5vw,10rem)] md:text-[clamp(2.5rem,4vw,10rem)] xl:text-[clamp(2rem,2.2vw,10rem)] 2xl:text-[clamp(2rem,2vw,10rem)]
+                    hover:bg-violet hover:text-hot-magenta
+                `}
                 href=""
                 onClick={handleLoginLinkClick}>Inicio</a>
 
             <a className={`
-                ${btnClasses}
-         
-                /* Tipografía */
-                text-[clamp(min(5vw,5rem),5vw,5rem)] 
-                sm:text-[clamp(min(4vw,10rem),4vw,10rem)] 
-                md:text-[clamp(min(2.5vw,10rem),3vw,10rem)]
-                xl:text-[clamp(min(1.6vw,10rem),1.6vw,10rem)]
-                2xl:text-[clamp(min(1.4vw,10rem),1.4vw,10rem)]
-
-                /* Hover */
-                hover:bg-canary hover:text-violet
-            `}
+                    ${btnClasses}
+                    text-[clamp(1rem,5vw,10rem)] sm:text-[clamp(5rem,5vw,10rem)] md:text-[clamp(1.8rem,3vw,10rem)] xl:text-[clamp(1rem,1.8vw,10rem)] 2xl:text-[clamp(1rem,1.6vw,10rem)]
+                    hover:bg-canary hover:text-violet
+                `}
                 href=""
                 onClick={handleRegisterLinkClick}>Registro</a>
         </main>
