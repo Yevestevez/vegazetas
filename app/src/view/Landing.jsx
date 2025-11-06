71681d12c4e0802f2dec3ca293597032b9ffd4d8
Yevestevez <yevestevez.cg@gmail.com>
2025-11-06 12:16:34 +0100

    add new Footer component to Landing; improve Landing styles and accesibility

--- app/src/view/Landing.jsx
+++ app/src/view/Landing.jsx
@@ -1,5 +1,8 @@
+// import { FaGithub, FaLinkedin } from "react-icons/fa"
+
 import CircleButton from "./common/CircleButton"
 import LogoVegazetas from "./common/LogoVegazetas"
+import Footer from "./common/Footer"
 
 function Landing() {
     return <div className="flex flex-col min-h-screen min-w-screen items-center justify-center gap-8 xl:gap-12 bg-folly">
@@ -7,9 +10,9 @@ function Landing() {
             <LogoVegazetas className="text-spring-bud" />
         </header>
 
-        <main className="flex flex-col gap-10 xl:gap-12">
-            <p className="text-center anybody-title text-spring-bud text-4xl lg:text-5xl xl:text-6xl">Cook & Cool
-            </p>
+        <main className="flex flex-col gap-10 xl:gap-12 mb-16">
+            <h1 className="text-center anybody-title text-spring-bud text-4xl lg:text-5xl xl:text-6xl">Cook & Cool</h1>
+
             <nav className="flex flex-col sm:flex-row items-center gap-6 sm:gap-8 lg:gap-10 xl:gap-12">
                 <CircleButton
                     to="/login"
@@ -22,6 +25,8 @@ function Landing() {
                 >Registro</CircleButton>
             </nav>
         </main>
+
+        <Footer />
     </div>
 }
 
--- app/src/view/common/LogoVegazetas.jsx
+++ app/src/view/common/LogoVegazetas.jsx
@@ -7,7 +7,7 @@ function LogoVegazetas({
 }) {
     const logoClasses = clsx(
         'text-center anybody-logo',
-        'text-8xl sm:text-9 lg:text-[12rem] xl:text-[14rem]',
+        'text-8xl sm:text-9xl lg:text-[12rem] xl:text-[14rem]',
         'leading-[0.8] sm:leading-[0.8] p-4',
         'drop-shadow-[0.6rem_0.6rem_0_rgba(0,0,0,0.8)]',
         className
