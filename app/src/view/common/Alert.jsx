465bbbd598ca7d433d3b0075abdc3ccd6f77be18
Yevestevez <yevestevez.cg@gmail.com>
2025-11-26 11:03:35 +0100

    minor css improvements in Alert, CircleButton and Confirm components

--- app/src/view/common/Alert.jsx
+++ app/src/view/common/Alert.jsx
@@ -1,12 +1,15 @@
 import CircleButton from "./CircleButton"
 
 function Alert({ message, onAccept, isWakingServer = false }) {
-    return <div className="fixed top-0 z-[100] flex items-center justify-center w-full h-full bg-folly bg-opacity-70">
+    return <div className="fixed inset-0 z-[100] flex items-center justify-center bg-folly bg-opacity-70">
         <div className="
-            flex flex-col items-center gap-4 xs:gap-8 p-4 xs:p-10 md:p-24 lg:p-48
-            mx-6 xs:mx-12 md:mx-16 lg:mx-24 xl:mx-36 max-w-7xl text-center 
+            flex flex-col items-center gap-6 xs:gap-8 lg:gap-10 xl:gap-12
+            p-6 xs:p-10 md:p-14 xl:p-20
+            mx-6 xs:mx-12 sm:mx-14 xl:mx-20
+            w-full max-w-xl
+            text-center 
             border-8 bg-folly border-spring-bud text-spring-bud
-            shadow-[0.8rem_0.8rem_0_0_rgba(0,0,0,0.8)]
+            shadow-[0.6rem_0.6rem_0_0_rgba(0,0,0,0.8)] lg:shadow-[0.8rem_0.8rem_0_0_rgba(0,0,0,0.8)]
         ">
             {isWakingServer && (
                 <svg
@@ -19,10 +22,10 @@ function Alert({ message, onAccept, isWakingServer = false }) {
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
                 </svg>
             )}
-            <p className="text-lg xs:text-2xl font-black leading-6 anybody">{message}</p>
+            <p className="text-base xs:text-lg md:text-xl xl:text-2xl font-black leading-tight xs:leading-tight md:leading-tight xl:leading-tight">{message}</p>
 
             <CircleButton
-                onClick={onAccept}
+                onClick={onAccept} variant="small"
                 className="bg-spring-bud text-folly outline-none hover:bg-folly hover:text-spring-bud hover:outline hover:outline-4 hover:outline-offset-0 hover:outline-spring-bud"
             >
                 Aceptar
